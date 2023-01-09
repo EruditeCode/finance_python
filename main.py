2545d1298414fda4738974a896841a88175c1696
EruditeCode <erudite.home@gmail.com>
2023-01-09 14:06:13 +0000

    Testing the instantiation of the StockAlert objects.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import pygame
 from sys import exit
+from class_StockAlert import StockAlert
 
 def main():
 	pygame.init()
@@ -12,6 +13,21 @@ def main():
 
 	bg = pygame.Surface((WIDTH, HEIGHT))
 	bg.fill((20,20,20))
+
+	# Create the stock alert objects.
+	stocks = {
+		"GOOGL":["Alphabet Ltd.", 88.00],
+		"AAPL":["Apple Inc.", 125.00],
+		"TSLA":["Tesla Inc.", 100.00],
+		"MSFT":["Microsoft Corp.", 200.00]
+	}
+	alerts = []
+	for key in stocks.keys():
+		alerts.append(StockAlert(key, stocks[key][0], stocks[key][1]))
+
+	for alert in alerts:
+		print(alert.name)
+	exit()
 
 	while True:
 		for event in pygame.event.get():
