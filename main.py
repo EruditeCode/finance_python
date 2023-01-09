76ab7e624ff4db9c79842b26ed36e66bd08e9b7a
EruditeCode <erudite.home@gmail.com>
2023-01-09 14:04:19 +0000

    Basic setup for the main file with pygame visualisation template.

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 # A program to generate alerts when stocks trade at set prices.
 
+import pygame
+from sys import exit
+
+def main():
+	pygame.init()
+	WIDTH, HEIGHT = 400, 600
+	screen = pygame.display.set_mode((WIDTH, HEIGHT))
+	pygame.display.set_caption("Stock Alert Tester")
+	clock = pygame.time.Clock()
+
+	bg = pygame.Surface((WIDTH, HEIGHT))
+	bg.fill((20,20,20))
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit()
+
+		# DISPLAY OBJECTS
+		screen.blit(bg, (0, 0))
+
+
+		# UPDATE OBJECTS
+
+		pygame.display.update()
+		clock.tick(1)
+
+
+if __name__ == "__main__":
+	main()
