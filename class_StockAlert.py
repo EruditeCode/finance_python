8accee4c2cf4ed4836f12a1b881b9508a6d45b7c
EruditeCode <erudite.home@gmail.com>
2023-01-09 13:57:42 +0000

    Created the StockAlert class with init function.

--- class_StockAlert.py
+++ class_StockAlert.py
@@ -1,2 +1,10 @@
 # Support class for finance programs.
 
+import yfinance as yf
+
+class StockAlert:
+	def __init__(self, symbol:str, name:str, alert_price:float):
+		self.symbol = symbol
+		self.name = name
+		self.alert_price = alert_price
+		self.status = False
